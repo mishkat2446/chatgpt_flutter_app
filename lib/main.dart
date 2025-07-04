110ef17da5ea8c3f5b78fa3e851b783d583b97f7
mishkat2446 <mishkatjsr0508@gmail.com>
2025-07-04 07:39:29 +0600

    🐛 Fix: Updated main.dart and pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:chatgpt_ai/chatgpt_ai.dart';
+import 'package:openai/openai.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
